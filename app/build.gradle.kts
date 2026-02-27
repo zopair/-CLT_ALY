3602295430c2688380e447aa9e1103e54d43656f
Aly <alyzopairaly@gmail.com>
2026-02-27 05:42:08 +0200

    CLT-SHA: Structural finalization of app/build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,25 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
 }
 android {
     namespace = "com.clt.aly"
-    compileSdk = 34
+    compileSdk = 33
     defaultConfig {
         applicationId = "com.clt.aly"
-        minSdk = 26
-        targetSdk = 34
+        minSdk = 24
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
     }
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+}
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
 }
